52e3692e3d3a7fcfa3e9508919c587ac9547478e
MagnusFlaa <magnfl@stud.ntnu.no>
2025-02-27 09:38:31 +0100

    Gjorde slik at main bare kjører start()

--- lab_2-0.1.0/skeleton_project/source/main.c
+++ lab_2-0.1.0/skeleton_project/source/main.c
@@ -3,6 +3,7 @@
 #include <signal.h>
 #include <time.h>
 #include "driver/elevio.h"
+#include "start.h"
 
 
 // make the elevator go to the bottom floor
@@ -16,59 +17,11 @@ int main(){
     printf("=== Example Program ===\n");
     printf("Press the stop button on the elevator panel to exit\n");
 
-    goTo0();
-    // For å signalisere at den har funnet stedet sitt.
-    elevio_stopLamp(1);  // Tenn stopp-lampa
-    nanosleep(&(struct timespec){1, 0}, NULL);  // Vent 1 sekund
-    elevio_stopLamp(0); //Slukk lampa
-    while(1){
-        elevio_floorIndicator(3);
-        int floor = elevio_floorSensor();
-
-        if(floor == 0){
-            elevio_motorDirection(DIRN_UP);
-        }
-
-        if(floor == N_FLOORS-1){
-            elevio_motorDirection(DIRN_DOWN);
-        }
-
-
-        for(int f = 0; f < N_FLOORS; f++){
-            for(int b = 0; b < N_BUTTONS; b++){
-                int btnPressed = elevio_callButton(f, b);
-                elevio_buttonLamp(f, b, btnPressed);
-            }
-        }
-
-        if(elevio_obstruction()){
-            elevio_stopLamp(1);
-        } else {
-            elevio_stopLamp(0);
-        }
-        
-        if(elevio_stopButton()){
-            elevio_motorDirection(DIRN_STOP);
-            break;
-        }
-        
-        nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
-    }
+    start();
 
     return 0;
 }
 
 
 
-void goTo0(){
-    elevio_motorDirection(DIRN_DOWN);
-    while(1){
-        int floor = elevio_floorSensor();
-        if (floor == 0){
-            elevio_motorDirection(DIRN_STOP);
-            break;
-        }
-        nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
-    }
 
-}
